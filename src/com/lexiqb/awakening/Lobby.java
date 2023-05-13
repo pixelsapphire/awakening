392a2e6b6e1f2cfee91c5935faf0ecefb65883e1
RubyNaxela <rubynaxela@gmail.com>
2023-05-13 07:59:14 +0200

    Update kyanite, add World & make Lobby its subclass

--- src/com/lexiqb/awakening/Lobby.java
+++ src/com/lexiqb/awakening/Lobby.java
@@ -1,43 +1,26 @@
 package com.lexiqb.awakening;
 
 import com.lexiqb.awakening.world.Portal;
-import com.rubynaxela.kyanite.game.Scene;
-import com.rubynaxela.kyanite.graphics.*;
-import com.rubynaxela.kyanite.math.Vec2;
+import com.lexiqb.awakening.world.World;
+import com.rubynaxela.kyanite.graphics.Colors;
+import com.rubynaxela.kyanite.graphics.Texture;
 
-public class Lobby extends Scene {
+public class Lobby extends World {
+
+    private final Portal testPortal = new Portal(Colors.ORANGE);
+
+    protected Lobby() {
+        super(2048, 2048, new Texture("assets/touch_sum_grass.png"));
+    }
 
     @Override
     protected void init() {
-        final var testPortal = new Portal(Colors.ORANGE);
         testPortal.setPosition(100, 100);
         add(testPortal);
-        final var bg = new RectangleShape(2048, 2048);
-        bg.setTexture(new Texture("assets/touch_sum_grass.png"));
-        add(bg);//yes
     }
 
-    @Override
-    protected void loop() {
-
-        // Moving objects
-//        for (final Drawable obj : getContext().getWindow().getScene()) {
-//            if (obj instanceof final MapObject gameObject) {
-//                gameObject.setPosition(Vec2.subtract(gameObject.getPositionOnMap(), mapOffset));
-//            }
-//            if (obj instanceof final GameText text) {
-//                text.setPosition(Vec2.subtract(text.getPositionOnMap(), mapOffset));
-//            }
-//            if (obj instanceof final MapBackground back) {
-//                float backgroundOffsetX, backgroundOffsetY;
-//                if (getMapSize().x > window.getSize().x) {
-//                    backgroundOffsetX = (back.mainBody.getSize().x - window.getSize().x) / (getMapSize().x - window.getSize().x);
-//                } else backgroundOffsetX = (back.mainBody.getSize().x - window.getSize().x) / getMapSize().x;
-//                if (getMapSize().y > window.getSize().y) {
-//                    backgroundOffsetY = (back.mainBody.getSize().y - window.getSize().y) / (getMapSize().y - window.getSize().y);
-//                } else backgroundOffsetY = (back.mainBody.getSize().y - window.getSize().y) / getMapSize().y;
-//                back.setPosition(Vec2.multiply(Vec2.subtract(back.getPositionOnMap(), Vec2.f(backgroundOffsetX, backgroundOffsetY)), getMapOffset()));
-//            }
-//        }
-    }
+//    @Override
+//    protected void loop() {
+//        super.loop();
+//    }
 }
---
+++ src/com/lexiqb/awakening/world/World.java
@@ -0,0 +1,61 @@
+package com.lexiqb.awakening.world;
+
+import com.lexiqb.awakening.entities.Player;
+import com.rubynaxela.kyanite.game.Scene;
+import com.rubynaxela.kyanite.graphics.*;
+import com.rubynaxela.kyanite.math.Vec2;
+import com.rubynaxela.kyanite.math.Vector2i;
+import com.rubynaxela.kyanite.window.Window;
+import org.jetbrains.annotations.NotNull;
+
+public abstract class World extends Scene {
+
+    private final Vector2i size;
+    private final Window window;
+    private Player player;
+
+    protected World(Vector2i size, @NotNull Texture background) {
+        this(size.x, size.y, background);
+    }
+
+    protected World(int width, int height, @NotNull Texture background) {
+        size = Vec2.i(width, height);
+        window = getContext().getWindow();
+        setOrderingPolicy(OrderingPolicy.Y_BASED);
+        final var backgroundShep = new RectangleShape(width, height);
+        backgroundShep.setTexture(background);
+        backgroundShep.setLayer(-1);
+        add(backgroundShep);
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void attachPlayer(@NotNull Player player) {
+        this.player = player;
+        add(player);
+    }
+
+    @Override
+    protected void loop() {
+
+        final var playerPosition = player.getPosition();
+        final var windowSize = window.getSize();
+        final boolean followOnX = playerPosition.x > windowSize.x / 2f && playerPosition.x < size.x - windowSize.x / 2f;
+        final boolean followOnY = playerPosition.y > windowSize.y / 2f && playerPosition.y < size.y - windowSize.y / 2f;
+
+        final ConstView view = getContext().getWindow().getView();
+        final var playerCenter = player.getGlobalBounds().getCenter();
+        float viewCenterX = view.getCenter().x, viewCenterY = view.getCenter().y;
+        if (followOnX) viewCenterX = playerCenter.x;
+        if (followOnY) viewCenterY = playerCenter.y;
+        ((View) view).setCenter(Vec2.f(viewCenterX, viewCenterY));
+        getContext().getWindow().setView(view);
+
+        player.update(getDeltaTime());
+
+        updateOrder();
+    }
+
+}
