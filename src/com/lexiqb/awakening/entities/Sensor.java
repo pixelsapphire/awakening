3782fd4333d8b23acab5a288a84b0e83141feb31
RubyNaxela <rubynaxela@gmail.com>
2023-05-13 21:14:16 +0200

    Add Sensor

--- src/com/lexiqb/awakening/entities/Sensor.java
+++ src/com/lexiqb/awakening/entities/Sensor.java
@@ -1,22 +1,22 @@
 package com.lexiqb.awakening.entities;
 
-import com.lexiqb.awakening.entities.Entity;
 import com.rubynaxela.kyanite.game.GameContext;
 import com.rubynaxela.kyanite.graphics.AnimatedTexture;
-import com.rubynaxela.kyanite.graphics.Colors;
+import com.rubynaxela.kyanite.math.Vec2;
 import com.rubynaxela.kyanite.util.Time;
 import org.jetbrains.annotations.NotNull;
 
 public class Sensor extends Entity {
+
     private float disturbedTime = 0.0f;
     private boolean disturbed = false;
 
     public Sensor() {
-        final var entranceTexture = GameContext.getInstance().getAssetsBundle().<AnimatedTexture>get("texture.world.portal");
+        final var texture = GameContext.getInstance().getAssetsBundle().<AnimatedTexture>get("texture.world.sensor");
 
-        setSize(40, 60);
-        setTexture(entranceTexture);
-        setFillColor(Colors.PURPLE);
+        setSize(Vec2.multiply(texture.getSize(), 4));
+        setTexture(texture);
+//        setFillColor(Colors.PURPLE);
         setOrigin(getSize().x / 2f, getSize().y);
         freezeAnimatedTexture();
     }
