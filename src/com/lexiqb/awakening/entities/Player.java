564fe8cf913195c70d14c87087c50eee16291653
DarkningPl <76476752+DarkningPl@users.noreply.github.com>
2023-05-13 12:40:10 +0200

    Implement jumps for Slime, change location of keepInWorldBounds and world-related functions for entities, move Obstacle class

--- src/com/lexiqb/awakening/entities/Player.java
+++ src/com/lexiqb/awakening/entities/Player.java
@@ -1,5 +1,7 @@
 package com.lexiqb.awakening.entities;
 
+import com.rubynaxela.kyanite.input.Keyboard;
+import com.rubynaxela.kyanite.math.Direction;
 import com.rubynaxela.kyanite.math.Vec2;
 import com.rubynaxela.kyanite.util.Time;
 import org.jetbrains.annotations.NotNull;
@@ -9,24 +11,10 @@ public class Player extends Slime {
     @Override
     public void update(@NotNull Time deltaTime) {
         super.update(deltaTime);
-        keepInWorldBounds(deltaTime);
     }
 
-    private void keepInWorldBounds(@NotNull Time deltaTime) {
-        final var gGB = getGlobalBounds();
-        assert getWorld() != null;
-        final var worldBounds = getWorld().getBounds();
-        float vX = getVelocity().x, vY = getVelocity().y;
-        // Horizontal map constraints
-        if (vX < 0 && gGB.left + vX * deltaTime.asSeconds() < 0)
-            vX = -gGB.left / deltaTime.asSeconds();
-        else if (vX > 0 && gGB.right + vX * deltaTime.asSeconds() > worldBounds.width)
-            vX = (worldBounds.width - gGB.right) / deltaTime.asSeconds();
-        // Vertical map constraints
-        if (vY < 0 && gGB.top + vY * deltaTime.asSeconds() < 0)
-            vY = -gGB.top / deltaTime.asSeconds();
-        else if (vY > 0 && gGB.bottom + vY * deltaTime.asSeconds() > worldBounds.height)
-            vY = (worldBounds.height - gGB.bottom) / deltaTime.asSeconds();
-        setVelocity(Vec2.f(vX, vY));
+    @Override
+    public Direction getDirection() {
+        return Keyboard.getDirection(Keyboard.StandardControls.WASD);
     }
 }
