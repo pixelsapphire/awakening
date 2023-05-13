750aa97990891363e76aba8c256e471c5d157ed9
RubyNaxela <rubynaxela@gmail.com>
2023-05-13 10:41:19 +0200

    Better assets structure, add portal gate

--- src/com/lexiqb/awakening/Awakening.java
+++ src/com/lexiqb/awakening/Awakening.java
@@ -21,17 +21,19 @@ public class Awakening extends Game {
     protected void preInit() {
         final AssetsBundle assets = getContext().getAssetsBundle();
 
-        assets.register("texture.ui.health", new Texture("assets/ui/health.png"));
-        assets.register("texture.ui.stamina", new Texture("assets/ui/stamina.png"));
-        assets.register("texture.ui.noise", new Texture("assets/ui/noise.png"));
+        assets.register("texture.ui.health", new Texture("assets/textures/ui/health.png"));
+        assets.register("texture.ui.stamina", new Texture("assets/textures/ui/stamina.png"));
+        assets.register("texture.ui.noise", new Texture("assets/textures/ui/noise.png"));
 
-        assets.register("texture.world.gate", new Texture("assets/gate.png"));
+        assets.register("texture.world.gate", new Texture("assets/textures/world/gate.png"));
 
-        assets.register("texture.entity.player", new TextureAtlas("assets/slime_atlas.png"));
+        assets.register("texture.world.lobby.background", new Texture("assets/textures/world/touch_sum_grass.png"));
+
+        assets.register("texture.entity.player", new TextureAtlas("assets/textures/entity/slime_atlas.png"));
 
         assets.register("texture.world.portal",
                         new AnimatedTexture(Stream.of(0, 1, 2, 3, 4, 5, 6, 7, 8)
-                                                  .map(i -> new Texture("assets/portal/stage" + i + ".png"))
+                                                  .map(i -> new Texture("assets/textures/world/portal/stage" + i + ".png"))
                                                   .toArray(ConstTexture[]::new), 1 / 9f));
     }
 
