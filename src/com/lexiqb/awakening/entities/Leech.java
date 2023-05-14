56c171b682ca020401eada88e7eb616d2b65c24b
RubyNaxela <rubynaxela@gmail.com>
2023-05-14 15:48:47 +0200

    Update Leech texture

--- src/com/lexiqb/awakening/entities/Leech.java
+++ src/com/lexiqb/awakening/entities/Leech.java
@@ -17,7 +17,7 @@ public class Leech extends Entity {
 
     private static final AudioHandler audioHandler = GameContext.getInstance().getAudioHandler();
     private static final AnimatedTexture animation = new AnimatedTexture(GameContext.getInstance().getAssetsBundle().<TextureAtlas>get("texture.entity.leech")
-                                                                                    .getRow(110, 110, 8), 0.2f);
+                                                                                    .getRow(128, 128, 12), 0.15f);
     private static final Sound roarSound = GameContext.getInstance().getAssetsBundle().get("sound.entity.leech.roar");
     private static final Sound nomSound = GameContext.getInstance().getAssetsBundle().get("sound.entity.leech.nom");
     private boolean awakened = false, nommingStarted = false, nommingEnded = false;
@@ -50,11 +50,11 @@ public class Leech extends Entity {
             if (eatingTime.asSeconds() >= 2 + animation.getFrameDuration() * animation.getFramesCount() * 0.9f) {
                 setTexture(animation.getFrame(animation.getFramesCount() - 1));
             }
-            if (!nommingStarted && eatingTime.asSeconds() >= 2 + animation.getFrameDuration() * 1) {
+            if (!nommingStarted && eatingTime.asSeconds() >= 2 + animation.getFrameDuration() * 2) {
                 audioHandler.playSound(nomSound, "environment", 100.0f, 1.0f, false);
                 nommingStarted = true;
             }
-            if (!nommingEnded && eatingTime.asSeconds() >= 2 + animation.getFrameDuration() * 3) {
+            if (!nommingEnded && eatingTime.asSeconds() >= 2 + animation.getFrameDuration() * 4) {
                 nommingEnded = true;
                 Objects.requireNonNull(food.getWorld()).stream().filter(o -> o instanceof GameObject && !(o instanceof Leech))
                        .forEach(o -> {
